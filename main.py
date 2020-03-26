72f32e915969d6364d1d1a335cd6e8775d165b4e
Opsy1169 <rodinilyaalexeevich@gmail.com>
2020-03-26 22:35:03 +0400

    erosion-dilatation swap, contours

--- main.py
+++ main.py
@@ -10,19 +10,20 @@ def main():
     binary = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     # cv2.imshow("1", binary)
     noise = add_noise(binary, 0.1)
-    print(noise_percentage(binary, noise))
-    open = opening(noise, shape='square', size=3)
-    close = closing(noise, shape='square', size=3)
-    table = filter_with_table(noise)
-    inner_contour = contour(binary, 'square', 3)
-    outter_contour = contour(binary, 'square', 3, inner=False)
-    hor_contour = oriented_contour(binary, orientation='horizontal')
-    ver_contour = oriented_contour(binary)
-    print(np.linalg.norm(inner_contour - inner_contour))
-    cv2.imshow("1", inner_contour)
-    cv2.imshow("2", outter_contour)
+    # er_3 = erosion(noise, shape='cross', size=3)
+    # print(noise_percentage(binary, noise))
+    # open = opening(noise, shape='square', size=3)
+    # close = closing(noise, shape='square', size=3)
+    # table = filter_with_table(noise)
+    # inner_contour = contour(binary, 'square', 3)
+    # outter_contour = contour(binary, 'square', 3, inner=False)
+    hor_contour = oriented_contour(binary)
+    # ver_contour = oriented_contour(binary)
+    # print(np.linalg.norm(inner_contour - inner_contour))
+    cv2.imshow("1", noise)
+    # cv2.imshow("2", outter_contour)
     cv2.imshow("3", hor_contour)
-    cv2.imshow("4", ver_contour)
+    # cv2.imshow("4", ver_contour)
     cv2.waitKey(0)
 
 
@@ -51,23 +52,22 @@ def erosion(image, shape='square', size=3):
     return result * 255
 
 
-def __dilatation(image, size, shape):
-    mask = get_square_mask(size) if shape == 'square' else get_cross_mask(size)
-    elements_in_mask = size ** 2 - 1 if shape == 'square' else 2 * (size - 1)
-    conv_result = signal.convolve2d(image, mask, mode='same', fillvalue=1)
-    for i in range(conv_result.shape[0]):
-        for j in range(conv_result.shape[1]):
-            conv_result[i][j] = 0 if conv_result[i][j] < elements_in_mask else 1
-    return conv_result
-
-
-def __erosion(image, size, shape):
-    mask = get_square_mask(size) if shape == 'square' else get_cross_mask(size)
+def __dilatation(image, size=3, shape='square', mask=None):
+    mask = (get_square_mask(size) if shape == 'square' else get_cross_mask(size)) if mask is None else mask
     conv_result = signal.convolve2d(image, mask, mode='same', fillvalue=0)
     for i in range(conv_result.shape[0]):
         for j in range(conv_result.shape[1]):
-            if conv_result[i][j] > 0:
-                conv_result[i][j] = 1
+            conv_result[i][j] = 1 if conv_result[i][j] > 0 else 0
+    return conv_result
+
+
+def __erosion(image, size=3, shape='square', mask=None):
+    mask = (get_square_mask(size) if shape == 'square' else get_cross_mask(size)) if mask is None else mask
+    conv_result = signal.convolve2d(image, mask, mode='same', fillvalue=1)
+    elements_in_mask = size ** 2 if shape == 'square' else 2 * size - 1
+    for i in range(conv_result.shape[0]):
+        for j in range(conv_result.shape[1]):
+            conv_result[i][j] = 0 if conv_result[i][j] < elements_in_mask else 1
     return conv_result
 
 
@@ -115,14 +115,11 @@ def get_cross_mask(size):
         for j in range(mask.shape[1]):
             if (i == size // 2) or (j == size // 2):
                 mask[i][j] = 1
-            if (i == size // 2) and (j == size // 2):
-                mask[i][j] = 0
     return mask
 
 
 def get_square_mask(size):
     mask = np.ones((size, size))
-    mask[size // 2][size // 2] = 0
     return mask
 
 
@@ -134,13 +131,11 @@ def contour(image, shape, size, inner=True):
 
 def oriented_contour(image, orientation='vertical'):
     image = image / 255
-    mask = np.array([[0, 1, 0], [0, 0, 0], [0, -1, 0]]) if orientation != 'vertical' else \
-        np.array([[0, 0, 0], [1, 0, -1], [0, 0, 0]])
-    conv_result = signal.convolve2d(image, mask, mode='same', fillvalue=1)
-    for i in range(conv_result.shape[0]):
-        for j in range(conv_result.shape[1]):
-            conv_result[i][j] = 0 if conv_result[i][j] == 0 else 1
-    return conv_result * 255
+    mask = np.array([[0, 1, 0], [0, 1, 0], [0, 1, 0]]) if orientation == 'vertical' else \
+        np.array([[0, 0, 0], [1, 1, 1], [0, 0, 0]])
+    result = __dilatation(image, mask=mask)
+    contour_result = (image + result) % 2 * 255
+    return contour_result
 
 
 main()
